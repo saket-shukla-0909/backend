737afaa8482cb3416b35bc0295951f14b9ef30e4
Saket Shukla <148642140+saket-shukla-0909@users.noreply.github.com>
2025-05-15 09:10:23 +0530

    get All User using admin and sub admin

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -120,7 +120,7 @@ const logoutUser = async (req, res) => {
 
 const getAllUser = async (req, res) => {
   try {
-    const users = await User.findAll(); 
+    const users = await User.find(); 
     res.status(200).json({ success: true, data: users });
   } catch (error) {
     console.error('Error fetching users:', error);
