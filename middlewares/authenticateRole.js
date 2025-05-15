9449bd53d33a978bc4604c583093ca2eb424f270
Saket Shukla <148642140+saket-shukla-0909@users.noreply.github.com>
2025-05-15 09:08:41 +0530

    get All User using admin and sub admin

--- middlewares/authenticateRole.js
+++ middlewares/authenticateRole.js
@@ -1,13 +1,14 @@
-// middleware/authorizeRole.js
 const roleMap = require("../utils/roles");
-
 
 const authorizeAdminOrSubAdmin = (req, res, next) => {
   const userRole = req.user.role;
-  if (!userRole || (roleMap[userRole] !== roleMap.admin || roleMap[userRole] !== roleMap['sub admin'])) {
-    return res.status(403).json({ success: false, message: 'Access denied' });
+    console.log(userRole, "this is userRole")
+  if (userRole === roleMap.admin || userRole === roleMap['sub admin']) {
+    console.log("this is true")
+    return next(); 
   }
-  next();
+
+  return res.status(403).json({ success: false, message: 'Access denied' });
 };
 
 module.exports = authorizeAdminOrSubAdmin;
